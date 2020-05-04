eeb5a16bada1c543881d7bfad8bb82ca773f5e18
sandun-win10 <sandun-win10@192.168.100.104>
2020-05-04 16:27:38 -0500

    Selenium Changes

--- cucmberprj/src/test/java/com/cucmber/mygit/cucmberprj/AppTest.java
+++ cucmberprj/src/test/java/com/cucmber/mygit/cucmberprj/AppTest.java
@@ -2,7 +2,12 @@ package com.cucmber.mygit.cucmberprj;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
@@ -16,6 +21,16 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
     	System.out.println("hello sandun ....................");
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("https://github.com/login");
+    	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    	driver.manage().window().maximize();
+    	
+    	driver.findElement(By.xpath("//input[@id='login_field']")).clear();
+    	driver.findElement(By.xpath("//input[@id='login_field']")).sendKeys("srabeyweera@gmail.com");
+    	driver.findElement(By.xpath("//input[@id='password']")).clear();
+    	driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Virtusa@2");
+    	driver.findElement(By.xpath("//input[@name='commit']")).click();
         assertTrue( true );
     }
 }
