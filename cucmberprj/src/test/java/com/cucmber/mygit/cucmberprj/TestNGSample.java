1450365ab9d4f1203839f567d60cba12ddb08990
sandun-win10 <sandun-win10@192.168.100.104>
2020-05-04 15:33:04 -0500

    TestNg modification

---
+++ cucmberprj/src/test/java/com/cucmber/mygit/cucmberprj/TestNGSample.java
@@ -0,0 +1,11 @@
+package com.cucmber.mygit.cucmberprj;
+
+import org.testng.annotations.Test;
+
+public class TestNGSample {
+
+	@Test
+	public void test() {
+		System.out.println("test....running.......TESTNG");
+	}
+}
